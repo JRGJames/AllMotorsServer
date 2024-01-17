a4cdc219149132235b22811410386fd7cd5975ca
JRGJames <jarogos14@gmail.com>
2024-01-17 13:44:13 +0100

    Updated ChatApi.java

--- src/main/java/alpha/allmotors/api/ChatApi.java
+++ src/main/java/alpha/allmotors/api/ChatApi.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,11 @@ public class ChatApi {
 
     @Autowired
     private ChatService chatService;
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getAllChats(@PathVariable("id") Long chatId) {
+        return ResponseEntity.ok(chatService.getChatById(chatId));
+    }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteChat(@PathVariable("id") Long chatId, @RequestParam("userId") UserEntity userId) {
